f53e8af5906bd768689cfc0f3c40280616fa6b88
Xinqi Jin <jxqhhh@gmail.com>
2019-06-12 00:17:38 -0300

    commit

--- csim.c
+++ csim.c
@@ -1,7 +1,15 @@
+/*
+Name:Xinqi Jin
+loginID:2016010524
+*/
 #include "cachelab.h"
+#include <iostream>
+using std::cout;
+using std::endl;
 
-int main()
+int main(int argc,char* argv[])
 {
     printSummary(0, 0, 0);
+    cout<<argc;
     return 0;
 }
