f7c2c178b7ce43221ffe646509f7f3b58c7438c7
Xinqi Jin <jxqhhh@gmail.com>
2019-06-18 04:18:39 -0300

    Parse the address

--- csim.c
+++ csim.c
@@ -12,6 +12,8 @@ loginID:2016010524
 
 typedef struct Node* PNode; 
 typedef struct Node { // Node of the double linked list.
+    int valid; // The valid bit.
+    char tag[64]; // The tag bits.
     PNode prev;
     PNode next;
 } Node;
@@ -79,7 +81,7 @@ int main( int argc,char* argv[] ) {
     /* Parse the trace file line by line. */
     FILE *fp = fopen( globalArgs.tracefile, "r" );
     char buf[1024];
-    DoubleLinkedList list;
+    DoubleLinkedList list; // Use a double linked list to simulate the cache.
     list.head = ( PNode )malloc( sizeof( Node ) );
     list.tail = ( PNode )malloc( sizeof( Node ) );
     list.head->prev = NULL;
@@ -87,10 +89,97 @@ int main( int argc,char* argv[] ) {
     list.tail->prev = list.head;
     list.tail->next = NULL;
     list.size = 0;
+    char address[64];
     while ( fgets( buf, sizeof(buf), fp ) != NULL ) {
         if( buf[0] == ' ' ){
-            if(list.size==0){
+            
+            int pos = 3; // position of the comma in this line of the trace file
+            while ( buf[pos]!= ',' ) {
+                pos ++;
+            }
 
+            memset( address, 0, sizeof(char)*64);
+            int pos2 = 60;
+            for( int i = pos - 1 ; i > 2 ; i-- ) {
+                switch ( buf[i] ) {
+                    case '0':
+                        break;
+                    case '1':
+                        address[ pos + 3 ] = 1;
+                        break;                       
+                    case '2':
+                        address[ pos + 2 ] = 1;
+                        break;
+                    case '3':
+                        address[ pos + 2 ] = 1;
+                        address[ pos + 3 ] = 1;
+                        break;
+                    case '4':
+                        address[ pos + 1 ] = 1;
+                        break;
+                    case '5':
+                        address[ pos + 1 ] = 1;
+                        address[ pos + 3 ] = 1;
+                        break;
+                    case '6':
+                        address[ pos + 1 ] = 1;
+                        address[ pos + 2 ] = 1;
+                        break;
+                    case '7':
+                        address[ pos + 1 ] = 1;
+                        address[ pos + 2 ] = 1;
+                        address[ pos + 3 ] = 1;
+                        break;   
+                    case '8':
+                        address[ pos ] = 1;
+                        break;
+                    case '9':
+                        address[ pos ] = 1;
+                        address[ pos + 3 ] = 1;
+                        break;                       
+                    case 'a':
+                        address[ pos ] = 1;
+                        address[ pos + 2 ] = 1;
+                        break;
+                    case 'b':
+                        address[ pos ] = 1;
+                        address[ pos + 2 ] = 1;
+                        address[ pos + 3 ] = 1;
+                        break;
+                    case 'c':
+                        address[ pos ] = 1;
+                        address[ pos + 1 ] = 1;
+                        break;
+                    case 'd':
+                        address[ pos ] = 1;
+                        address[ pos + 1 ] = 1;
+                        address[ pos + 3 ] = 1;
+                        break;
+                    case 'e':
+                        address[ pos ] = 1;
+                        address[ pos + 1 ] = 1;
+                        address[ pos + 2 ] = 1;
+                        break;
+                    case 'f':
+                        address[ pos ] = 1;
+                        address[ pos + 1 ] = 1;
+                        address[ pos + 2 ] = 1;
+                        address[ pos + 3 ] = 1;
+                        break;
+                    default:
+                        break;                                                                                                                                                                                       
+
+                }
+            }
+
+            PNode pointer = list.head->next;
+            int cached = 0;
+            while ( pointer != list.tail ) {
+                if( pointer->valid ) {
+                    cached=pos2;
+                    pos2=cached;
+                }
+                pointer = pointer->next;
             }
         }
     }
